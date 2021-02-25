1cfd107329e71c1d77c17cb5390a8b1a1b5c33c1
Srijanaki <anjani204@gmail.com>
2021-02-25 11:07:32 +0530

    some changes

--- blackjack.py
+++ blackjack.py
@@ -11,41 +11,35 @@ inita=a1+a2
   
 print("Numbers for A  : " +str(a1)+  "  " + str(a2)) 
 print("Numbers for B  : " +str(b1)+ "  " + str(b2)) 
-
-
-i=int(input("Mr B : Enter hit(1) or  stand(0)"))
-if i==1:
-    sumb=0
-    while True:
-         if sumb<21:
-            x = random.choice(test_list)
-            print("you got number",str(x)) 
-            sumb=initb+x
-            initb=sumb
-            print("Score of Mr.B=" ,sumb)
-         else:
-            if sumb==21:
-                print("you won")
-                break
-            else:
-                print("Busted")
-                break        
-if i==0:
-    suma=0
-    sumb=initb
-    while True:
-         if suma<17:
-             y = random.choice(test_list) 
-             print("you got number ",str(y)) 
-             suma=inita+y
-             inita=suma
-             print("Score of dealer=" ,suma)
-         else :
-            if suma > sumb and suma<21:
-                print("Dealer won")
-                break
-            elif suma ==  sumb:
-                print("Tie")
-                break
-            else:
-                print("busted")
+sumb=initb
+suma=inita
+while True:
+    i=int(input("Enter hit(1) or  stand(0)"))
+    if i ==1 and sumb<21:
+        x = random.choice(test_list)
+        print("you got number",str(x)) 
+        sumb=initb+x
+        initb=sumb
+        print("Score of Mr.B=" ,sumb)
+        if sumb >21:
+            print("busted")
+            break
+        elif sumb==21:
+            print("you won")
+            break               
+        
+    if i==0 and suma<18:
+        y = random.choice(test_list)
+        print("you got number",str(y)) 
+        suma=inita+y
+        inita=suma
+        print("Score of dealer=" ,sumb)
+        if sumb >21:
+            print(" dealer busted")
+            break
+        elif sumb==21:
+            print("dealerwon")
+            break 
+        elif suma == sumb:
+            print("Tie")
+     
